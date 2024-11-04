641e627e80920aaba1948998dec5ec16ae8da321
KotaYoshioka <supakouta@gmail.com>
2024-11-04 10:17:46 +0900

    簡易的なマッチング画面の実装

--- Arcane/Assets/Scripts/GameManager.cs
+++ Arcane/Assets/Scripts/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        netWorkManager.StartGame();
+        // netWorkManager.StartGame();
         boardManager.SetBoard();
         boardManager.SetPiece(Instantiate(piecePrefab),5,5);
     }
---
+++ Arcane/Assets/Scripts/SceneScripts/Matching/MatchingManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fusion;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// マッチング画面のUIに紐づくスクリプト
+/// 基本的なマッチングの操作を行う
+/// </summary>
+public class MatchingManager : MonoBehaviour
+{
+    // テキストフィールド
+    [SerializeField] private TMP_InputField _usernameField;
+    [SerializeField] private TMP_InputField _roomnameField;
+
+    [SerializeField] private NetworkRunner _networkRunnerPrefab;
+    private NetworkRunner _runnerInstance = null;
+
+    /// <summary>
+    ///  フィールドに入力された情報を基に、マッチングを開始する
+    /// </summary>
+    public async void StartGame()
+    {
+        // NetworkRunnerを立ち上げる
+        _runnerInstance = FindObjectOfType<NetworkRunner>();
+        if(_runnerInstance == null){
+            _runnerInstance = Instantiate(_networkRunnerPrefab);
+        }
+
+        // _runnerInstance.ProvideInput = true;
+
+        Debug.Log(_roomnameField.text);
+
+        // オンラインの設定を行う
+        var startGameArgs = new StartGameArgs(){
+            GameMode = GameMode.Shared,
+            SessionName = _roomnameField.text,
+            Scene = SceneRef.FromIndex(SceneUtility.GetBuildIndexByScenePath("Scenes/GameScene")),
+        };
+
+        await _runnerInstance.StartGame(startGameArgs);
+    }
+
+}
