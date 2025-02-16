2831adff51cc8a28d773b81a896cb62fdac891da
GODkamiya <garlicgame@outlook.jp>
2025-02-16 20:56:58 +0900

    選択中の駒の色変更

--- Arcane/Assets/Scripts/SceneScripts/GameScene/ClickAction/PieceMoveClickAction.cs
+++ Arcane/Assets/Scripts/SceneScripts/GameScene/ClickAction/PieceMoveClickAction.cs
@@ -9,6 +9,14 @@ public class PieceMoveClickAction : IClickAction
         if (latestPiece == null) return;
         if (!latestMove.range[bb.x, bb.y]) return;
         if (BoardManager.singleton.onlinePieces[bb.x, bb.y] != null && BoardManager.singleton.onlinePieces[bb.x, bb.y].GetComponent<PieceObject>().isMine) return;
+        if (latestPiece.GetComponent<PieceObject>().isKing)
+        {
+            latestPiece.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            latestPiece.GetComponent<Renderer>().material.color = Color.white;
+        }
         latestPiece.GetComponent<PieceObject>().SetPosition(bb.x, bb.y, true);
         BoardManager.singleton.ClearMovement();
         GameManager.singleton.TurnEnd();
@@ -18,8 +26,20 @@ public class PieceMoveClickAction : IClickAction
     {
         PieceObject piece = pieceObject.GetComponent<PieceObject>();
         if (!piece.isMine) return;
-        if(piece.isSickness) return;
+        if (piece.isSickness) return;
+        if (latestPiece != null)
+        {
+            if (latestPiece.GetComponent<PieceObject>().isKing)
+            {
+                latestPiece.GetComponent<Renderer>().material.color = Color.red;
+            }
+            else
+            {
+                latestPiece.GetComponent<Renderer>().material.color = Color.white;
+            }
+        }
         latestPiece = pieceObject;
+        pieceObject.GetComponent<Renderer>().material.color = Color.cyan;
         PieceMovement move = piece.GetPieceMovement();
         latestMove = move;
         BoardManager.singleton.ShowMovement(move);
