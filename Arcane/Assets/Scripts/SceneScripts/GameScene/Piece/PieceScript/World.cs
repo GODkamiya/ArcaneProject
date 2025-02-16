2fb8f39e7a3093fadeae59bb2533d9f05d6fc4da
GODkamiya <garlicgame@outlook.jp>
2025-02-16 19:28:12 +0900

    ワールドの手札戻り

--- Arcane/Assets/Scripts/SceneScripts/GameScene/Piece/PieceScript/World.cs
+++ Arcane/Assets/Scripts/SceneScripts/GameScene/Piece/PieceScript/World.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
 
-public class World : PieceObject
+public class World : PieceObject, IOnAttackEvent
 {
     public override string GetName()
     {
         return "World";
     }
 
-    public override PieceMovement GetPieceMovementOrigin(int baseX,int baseY)
+    public override PieceMovement GetPieceMovementOrigin(int baseX, int baseY)
     {
         PieceMovement pm = new PieceMovement();
         for (int addX = -1; addX < 2; addX++)
         {
             for (int addY = -1; addY < 2; addY++)
             {
-                if(addX == 0 && addY == 0)continue;
+                if (addX == 0 && addY == 0) continue;
                 pm.AddRange(baseX + addX, baseY + addY);
             }
         }
@@ -24,5 +24,16 @@ public class World : PieceObject
     public override PieceType GetPieceType()
     {
         return PieceType.World;
+    }
+
+    public void OnAttack(int newX, int newY)
+    {
+        // 既存のコマを削除し、手札に戻す
+        Death();
+        PlayerObject po = GameManager.singleton.GetLocalPlayerObject();
+        if (isMine)
+        {
+            po.AddHand(PieceType.World);
+        }
     }
 }
