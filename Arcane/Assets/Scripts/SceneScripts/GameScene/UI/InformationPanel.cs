552dc235b211539465591bdf2b579415321ce6c7
KotaYoshioka <supakouta@gmail.com>
2025-03-30 18:57:56 +0900

    ログをとれるような素体の用意

---
+++ Arcane/Assets/Scripts/SceneScripts/GameScene/UI/InformationPanel.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+/// <summary>
+/// ゲーム画面左側に表示されている情報パネルの全体を統括するクラス
+/// </summary>
+public class InformationPanel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject descriptionPanel,logPanel;
+
+    /// <summary>
+    /// 説明ボタンが押されたときの挙動
+    /// 説明パネルを表示し、ログパネルを閉じる
+    /// </summary>
+    public void OnClickDescriptionButton(){
+        descriptionPanel.SetActive(true);
+        logPanel.SetActive(false);
+    }
+
+    /// <summary>
+    /// ログボタンが押されたときの挙動
+    /// ログパネルを表示し、説明パネルを閉じる
+    /// </summary>
+    public void OnClickLogPanel(){
+        descriptionPanel.SetActive(false);
+        logPanel.SetActive(true);
+    }
+}
---
+++ Arcane/Assets/Scripts/SceneScripts/GameScene/UI/LogPanel.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+public class LogPanel : MonoBehaviour
+{
+    /// <summary>
+    /// ログのオブジェクトを収納するスペース
+    /// </summary>
+    [SerializeField]
+    private GameObject logFolder;
+
+    [SerializeField]
+    GameObject logPrefab;
+
+    public void AddLog(LogBase log){
+        GameObject logObject = Instantiate(logPrefab);
+        logObject.GetComponentInChildren<TextMeshProUGUI>().text = log.GetLogMessage();
+    }
+}
